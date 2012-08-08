54ea5469d10bdf01dfe8c3bfcbc938d4a870aecd
fengcunhan <fengcunhan@126.com>
2012-08-08 22:30:50 +0800

    Add Hello.java

---
+++ src/com/learn/basic/Hello.java
@@ -0,0 +1,17 @@
+package com.learn.basic;
+
+/**
+ * @author feng
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello,ÀîÏş¾ü");
+
+	}
+
+}
