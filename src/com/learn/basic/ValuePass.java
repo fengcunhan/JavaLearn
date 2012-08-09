a13b971e3435abfe480d908b64411d6782fd2c56
fengcunhan <fengcunhan@126.com>
2012-08-09 13:34:08 +0800

    add pass ref

--- src/com/learn/basic/ValuePass.java
+++ src/com/learn/basic/ValuePass.java
@@ -1,5 +1,7 @@
 package com.learn.basic;
 
+import com.learn.basic.model.Person;
+
 /**
  * 彻底搞清楚在Java里面到底是传值还是传引用
  * 结论：对于基本数据类型，是传值（String除外，因为String对象是不可变的），除此之外的都是传递引用
@@ -14,7 +16,7 @@ public static void main(String[] args) {
 	
 	float addValue=add(a,a);
 	
-	//如果传递的是引用，那么在做完加法后，a的值应该是19
+	//如果传递的是引用，那么在做完加法后，a的值应该是18
 	System.out.println("value of a:"+a);
 	System.out.println("value of addValue:"+addValue);
 	if(a==18){
@@ -22,7 +24,17 @@ public static void main(String[] args) {
 	}else{
 		System.out.println(false);
 	}
+	//测试传递引用
+	Person person=new Person();
+	person.setAge(20);
+	person.setName("jary li");
 	
+	Person p=changeAge(person, 40);
+	if((p.toString().equals(person.toString()))){
+		System.out.println(true);
+	}else{
+		System.out.println(false);
+	}
 	
 }
 
@@ -30,5 +42,11 @@ public static void main(String[] args) {
 public static float add(float a,float b){
 	a=a+b;
 	return a;
+}
+
+public static Person changeAge(Person p,int newAge){
+	Person person=p;
+	person.setAge(newAge);
+	return person;
 }
 }
